c6c46572fd6e317d0cfa73aebdbcc779b3c404f8
Edwin Neef <edwinneef@gmail.com>
2019-10-04 14:05:03 +0200

    Create menu item function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const baseUrl = 'http://localhost:3000/menu_items'
+
+function create(data) {
+    let options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+    return fetch(baseUrl, options)
+      .then((response) => response.json())
+  }
+
 class App extends React.Component{
     render(){
+        
+        // create({ title: 'Test item', url: '#' })
+
         return(
             <div>Hello World</div>
         )
