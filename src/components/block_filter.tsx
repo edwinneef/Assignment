df8866a17a4c7288cafc29746b9d4b29b449f1ec
Edwin Neef <edwinneef@gmail.com>
2019-10-09 19:00:01 +0200

    Responsive fixes

--- src/components/block_filter.tsx
+++ src/components/block_filter.tsx
@@ -20,87 +20,97 @@ function Filter(props: FilterProps): JSX.Element {
   return (
     <div className="case-filter">
       <div className="case-filter__inner container">
-        <span className="case-filter__description">Show me</span>
-        {props.categories && (
-          <ul className="case-filter__menu case-filter__menu--category">
-            <li className="case-filter__menu-item">
-              <span className="case-filter__all">{filter.currentCategory}</span>
-              <ul className="case-filter__dropdown">
-                {filter.currentCategory != "All categories" && (
-                  <li
-                    className="case-filter__dropdown-item"
-                    onClick={() => {
-                      props.filterParent("all", undefined);
-                      setFilter({
-                        ...filter,
-                        currentCategory: "All categories"
-                      });
-                    }}
-                  >
-                    All categories
-                  </li>
-                )}
-                {props.categories.map((c, i) => {
-                  const title = c.charAt(0).toUpperCase() + c.slice(1);
-                  return filter.currentCategory != title ? (
+        <div className="case-filter__row">
+          <span className="case-filter__description">Show me</span>
+          {props.categories && (
+            <ul className="case-filter__menu case-filter__menu--category">
+              <li className="case-filter__menu-item">
+                <span className="case-filter__all">
+                  {filter.currentCategory}
+                </span>
+                <ul className="case-filter__dropdown">
+                  {filter.currentCategory != "All categories" && (
                     <li
                       className="case-filter__dropdown-item"
                       onClick={() => {
-                        props.filterParent(c, undefined);
-                        setFilter({ ...filter, currentCategory: title });
-                      }}
-                      key={i}
-                    >
-                      {title}
-                    </li>
-                  ) : null;
-                })}
-              </ul>
-            </li>
-          </ul>
-        )}
-        <span className="case-filter__description">in</span>
-        {props.industries && (
-          <ul className="case-filter__menu case-filter__menu--industries">
-            <li className="case-filter__menu-item">
-              <span className="case-filter__all">{filter.currentIndustry}</span>
-              <ul className="case-filter__dropdown">
-                {filter.currentIndustry != "All industries" && (
-                  <li
-                    className="case-filter__dropdown-item"
-                    onClick={() => {
-                      props.filterParent(undefined, "all");
-                      setFilter({
-                        ...filter,
-                        currentIndustry: "All industries"
-                      });
-                    }}
-                  >
-                    All industries
-                  </li>
-                )}
-                {props.industries.map((c, i) => {
-                  const title = c.charAt(0).toUpperCase() + c.slice(1);
-                  return filter.currentIndustry != title ? (
-                    <li
-                      className="case-filter__dropdown-item"
-                      onClick={() => {
-                        props.filterParent(undefined, c);
+                        props.filterParent("all", undefined);
                         setFilter({
                           ...filter,
-                          currentIndustry: title
+                          currentCategory: "All categories"
                         });
                       }}
-                      key={i}
                     >
-                      {c.charAt(0).toUpperCase() + c.slice(1)}
+                      All categories
                     </li>
-                  ) : null;
-                })}
-              </ul>
-            </li>
-          </ul>
-        )}
+                  )}
+                  {props.categories.map((c, i) => {
+                    const title = c.charAt(0).toUpperCase() + c.slice(1);
+                    return filter.currentCategory != title ? (
+                      <li
+                        className="case-filter__dropdown-item"
+                        onClick={() => {
+                          props.filterParent(c, undefined);
+                          setFilter({ ...filter, currentCategory: title });
+                        }}
+                        key={i}
+                      >
+                        {title}
+                      </li>
+                    ) : null;
+                  })}
+                </ul>
+              </li>
+            </ul>
+          )}
+        </div>
+
+        <div className="case-filter__row">
+          <span className="case-filter__description">in</span>
+
+          {props.industries && (
+            <ul className="case-filter__menu case-filter__menu--industries">
+              <li className="case-filter__menu-item">
+                <span className="case-filter__all">
+                  {filter.currentIndustry}
+                </span>
+                <ul className="case-filter__dropdown">
+                  {filter.currentIndustry != "All industries" && (
+                    <li
+                      className="case-filter__dropdown-item"
+                      onClick={() => {
+                        props.filterParent(undefined, "all");
+                        setFilter({
+                          ...filter,
+                          currentIndustry: "All industries"
+                        });
+                      }}
+                    >
+                      All industries
+                    </li>
+                  )}
+                  {props.industries.map((c, i) => {
+                    const title = c.charAt(0).toUpperCase() + c.slice(1);
+                    return filter.currentIndustry != title ? (
+                      <li
+                        className="case-filter__dropdown-item"
+                        onClick={() => {
+                          props.filterParent(undefined, c);
+                          setFilter({
+                            ...filter,
+                            currentIndustry: title
+                          });
+                        }}
+                        key={i}
+                      >
+                        {c.charAt(0).toUpperCase() + c.slice(1)}
+                      </li>
+                    ) : null;
+                  })}
+                </ul>
+              </li>
+            </ul>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/components/block_header.tsx
+++ src/components/block_header.tsx
@@ -3,8 +3,15 @@ import * as React from "react";
 function Header(): JSX.Element {
   return (
     <div className="header--main">
-      <div className="container">
+      <div className="header--main__inner container">
         <h1 className="header--main__title">Work</h1>
+      </div>
+      <div className="header--main__visual">
+        <img
+          className="header--main__image"
+          src="https://picsum.photos/id/4/1920/1080"
+          alt=""
+        />
       </div>
     </div>
   );
